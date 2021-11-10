4c304951b1dfe899532d904e2bb780fa6f20cc34
Jackson Morgan <67020956+jpmorgan98@users.noreply.github.com>
2021-11-10 14:14:22 -0800

    culling the dead 1st attempt at multi region
    problem with fission

--- serial/testbed_main.py
+++ serial/testbed_main.py
@@ -7,40 +7,58 @@ Author: Jackson Morgan (OR State Univ - morgjack@oregonstate.edu) CEMeNT
 Date: Nov 9th 2021
 current implemented physics:
         -slab geometry
+        -multiregion (only one generating)
         -monoenergtic
-        -single region
         -isotropic or uniform source direction
         -fission/capture/scatter region
-        
+        -purging the dead
 """
 
 import numpy as np
 import math
 
 #===============================================================================
-# Simulation settings
+# Simulation settings (input deck)
 #===============================================================================
 
 seed = 777
-num_part = 1000 #number of particles to start
+num_part = 100000 #number of particles to start
+init_particle = num_part
 particle_speed = 1 #particle speed
 
-cap_xsec = 1/3 #capture crossection
-scat_xsec = 1/3 #scattering crossection
-fis_xsec = 1/3 #fission crossection
-abs_xsec = cap_xsec+fis_xsec #absorption crossection
-total_xsec = abs_xsec+scat_xsec #total crossection
-
-generations = 10
+generations = 1
 nu_new_neutrons = 2 #neutrons/fission
 isotropic = 1 #is it isotropic 1=yes 0=no
 
-L=2.0
+# Test case 1
+cap_xsec = np.array([1/3]) #capture crossection
+scat_xsec = np.array([1/3]) #scattering crossection
+fis_xsec = np.array([1/3]) #fission crossection
+
+#abs_xsec = cap_xsec+fis_xsec #absorption crossection
+total_xsec = np.array([1]) #total crossection
+
+L = [0,2] #x coordinants of boundaries
+generation_region = 0
+regions = 1
+
+# Test case 2
+#index refers to region
+# cap_xsec = np.array([1/3, 1/3, 1/3]) #capture crossection
+# scat_xsec = np.array([2/3, 1/3, 2/3]) #scattering crossection
+# fis_xsec = np.array([0,1/3,0]) #fission crossection
+
+# #abs_xsec = cap_xsec+fis_xsec #absorption crossection
+# total_xsec = np.array([1,1,1]) #total crossection
+#L = [0,2,4,5] #coordinants of boundaries
+#generation_region = 1
+# regions = 3
 
 #===============================================================================
 # Allocate particle phase space
 #===============================================================================
-phase_parts = num_part
+
+phase_parts = 5*num_part
 
 # Position
 p_pos_x = np.zeros(phase_parts, dtype=np.float32)
@@ -57,6 +75,9 @@ p_speed = np.zeros(phase_parts, dtype=np.float32)
 
 # Time
 p_time = np.zeros(phase_parts, dtype=np.float32)
+
+# Region
+p_region = np.zeros(phase_parts, dtype=np.uint8)
 
 # Flags
 p_alive = np.full(phase_parts, False)
@@ -86,9 +107,13 @@ norm_fis = fis_xsec/total_xsec
 # EVENT 0 : Sample particle sources
 #===============================================================================
 
+L_gen = L[1]-L[0]
+x_rhs_gen = L[0]
+
 for i in range(num_part):
     # Position
-    p_pos_x[i] = L*np.random.random()
+    p_pos_x[i] = x_rhs_gen + L_gen*np.random.random()
+    p_region[i] = generation_region
     p_pos_y[i] = 0.0
     p_pos_z[i] = 0.0
 
@@ -122,8 +147,12 @@ for i in range(num_part):
 # Generation Loop
 #===============================================================================
 trans = 0
-alive_last = num_part
-for g in range(generations):
+g = 0
+alive = num_part
+total_particles_to_live = num_part
+trans_lhs = 0
+trans_rhs = 0
+while alive > 0:
     print("")
     print("===============================================================================")
     print("                             Generation {0}".format(g))
@@ -137,7 +166,7 @@ for g in range(generations):
     for i in range(num_part):
         if p_alive[i] == True:
             # Sample distance to collision
-            dist = -math.log(np.random.random())/total_xsec
+            dist = -math.log(np.random.random())/total_xsec[p_region[i]]
         
             # Move particle
             p_pos_x[i] += p_dir_x[i]*dist
@@ -148,23 +177,28 @@ for g in range(generations):
             p_time[i]  += dist/p_speed[i]
 
     #===============================================================================
-    # EVENT 2 : Still in problem?
+    # EVENT 2 : Still in problem/ update region
     #===============================================================================
     
     # Count if transmitted
     for i in range(num_part):
-        if p_pos_x[i]<0:
+        #leak left
+        if p_pos_x[i]<L[0]:
             if p_alive[i] == True:
                 p_alive[i]=False
                 p_event[i]=6
                 #killed += 1
-            
-        if p_pos_x[i]>L:
+        #leak right
+        elif p_pos_x[i]>L[1]:
             if p_alive[i] == True:
                 p_event[i] = 5
                 p_alive[i] = False
                 #killed += 1
-
+        else:
+            for l in range(regions):
+                if (L[l] < p_pos_x[i] < L[l+1]):
+                    p_region[i] = l
+                
 
     #===============================================================================
     # EVENT 3 : Sample event
@@ -176,17 +210,17 @@ for g in range(generations):
             event_rand = np.random.random()
             
             #scatter?
-            if event_rand < norm_scat:
+            if event_rand < norm_scat[p_region[i]]:
                 p_event[i] = 1
             
             #capture?
-            elif norm_scat < event_rand < norm_scat+norm_cap:
+            elif norm_scat[p_region[i]] < event_rand < norm_scat[p_region[i]]+norm_cap[p_region[i]]:
                 p_event[i] = 2
                 p_alive[i] = False
                 killed += 1
                 
             #fission?
-            elif norm_scat+norm_cap < event_rand < norm_scat+norm_cap+norm_fis:
+            elif norm_scat[p_region[i]]+norm_cap[p_region[i]] < event_rand < norm_scat[p_region[i]]+norm_cap[p_region[i]]+norm_fis[p_region[i]]:
                 p_event[i] = 4 #four means fission in this generation will require new particles
                 p_alive[i] = False
                 killed += 1
@@ -202,28 +236,6 @@ for g in range(generations):
     # EVENT 4: Generate fission particles
     #===============================================================================
     
-    #allocating new fission particle phase space
-    #doing as a two step process in order to minimize the number of append calls
-    
-    # Position
-    p_pos_x_fis = np.zeros(fissions_to_add, dtype=np.float32)
-    p_pos_y_fis = np.zeros(fissions_to_add, dtype=np.float32)
-    p_pos_z_fis = np.zeros(fissions_to_add, dtype=np.float32)
-    
-    # Direction
-    p_dir_x_fis = np.zeros(fissions_to_add, dtype=np.float32)
-    p_dir_y_fis = np.zeros(fissions_to_add, dtype=np.float32)
-    p_dir_z_fis = np.zeros(fissions_to_add, dtype=np.float32)
-    
-    # Speed
-    p_speed_fis = np.zeros(fissions_to_add, dtype=np.float32)
-    
-    # Time
-    p_time_fis = np.zeros(fissions_to_add, dtype=np.float32)
-    
-    # Flags
-    p_alive_fis = np.full(fissions_to_add, False)
-    p_event_fis = np.zeros(fissions_to_add, dtype=np.uint8)
 
     k=0 #index for fission temp vectors
     for i in range(num_part):
@@ -231,81 +243,106 @@ for g in range(generations):
             p_event[i] = 3 #fission in some last generation no particles to be generated in the future
             for j in range(nu_new_neutrons):
                 # Position
-                p_pos_x_fis[k] = p_pos_x[i]
-                p_pos_y_fis[k] = p_pos_y[i]
-                p_pos_z_fis[k] = p_pos_x[i]
-    
+                p_pos_x[k+num_part] = p_pos_x[i]
+                p_region[k+num_part] = p_region[i]
+                p_pos_y[k+num_part] = p_pos_y[i]
+                p_pos_z[k+num_part] = p_pos_x[i]
+                
                 # Direction
                 # Sample polar and azimuthal angles uniformly
                 mu  = 2.0*np.random.random() - 1.0
                 azi = 2.0*np.pi*np.random.random()
                 # Convert to Cartesian coordinate
                 c = (1.0 - mu**2)**0.5
-                p_dir_y_fis[k] = np.cos(azi)*c
-                p_dir_z_fis[k] = np.sin(azi)*c
-                p_dir_x_fis[k] = mu
+                p_dir_y[k+num_part] = np.cos(azi)*c
+                p_dir_z[k+num_part] = np.sin(azi)*c
+                p_dir_x[k+num_part] = mu
                       
                 # Speed
-                p_speed_fis[k] = particle_speed
+                p_speed[k+num_part] = particle_speed
                 
                 # Time
-                p_time_fis[k] = p_time[i]
+                p_time[k+num_part] = p_time[i]
     
                 # Flags
-                p_alive_fis[k] = True
+                p_alive[k+num_part] = True
                 
                 k+=1
                 
-    #Append full arrays with fission particles
-    p_pos_x = np.append(p_pos_x, p_pos_x_fis)
-    p_pos_y = np.append(p_pos_y, p_pos_y_fis)
-    p_pos_z = np.append(p_pos_z, p_pos_z_fis)
-    
-    p_dir_x = np.append(p_dir_x, p_dir_x_fis)
-    p_dir_y = np.append(p_dir_y, p_dir_y_fis)
-    p_dir_z = np.append(p_dir_z, p_dir_z_fis)
-    
-    p_speed = np.append(p_speed, p_speed_fis)
-    
-    p_time = np.append(p_time, p_time_fis)
-    
-    p_alive = np.append(p_alive, p_alive_fis)
-    p_event = np.append(p_event, p_event_fis)
-    
-    num_part += fissions_to_add
+    num_part += k
+    total_particles_to_live += k
     
     #===============================================================================
     # Criticality & Output (not really an event)
     #===============================================================================
     
-    criticality = fissions_to_add/killed
-    print("k = {0} (birth/death)".format(criticality))
+    # criticality = fissions_to_add/killed
+    # print("k = {0} (birth/death)".format(criticality))
             
-    alive_now =0
+    # alive_now =0
+    # for i in range(num_part):
+    #     if p_alive[i] == True:
+    #         alive_now +=1
+    # print("k = {0} (pop now/pop last)".format(alive_now/alive_last))
+    
+    
+    #===============================================================================
+    # EVENT 10 : Tally score
+    #===============================================================================
+    for i in range(num_part):
+        if p_event[i] == 5:
+            trans_rhs += 1
+    for i in range(num_part):
+        if p_event[i] == 6:
+            trans_lhs += 1
+    
+    
+    #===============================================================================
+    # Event 5: Purge the dead
+    #===============================================================================
+    kept = 0
     for i in range(num_part):
         if p_alive[i] == True:
-            alive_now +=1
-    print("k = {0} (pop now/pop last)".format(alive_now/alive_last))
-    
-    print("population start: {0}".format(alive_last))
-    print("population end: {0}".format(alive_now))
+            p_pos_x[k] = p_pos_x[i]
+            p_pos_y[k] = p_pos_y[i]
+            p_pos_z[k] = p_pos_z[i]
+            
+            # Direction
+            p_dir_x[k] = p_dir_x[i]
+            p_dir_y[k] = p_dir_y[i]
+            p_dir_z[k] = p_dir_z[i]
+            
+            # Speed
+            p_speed[k] = p_speed[i]
+            
+            # Time
+            p_time[k] = p_time[i]
+            
+            # Regions
+            p_region[k] = p_region[i]
+            
+            # Flags
+            p_alive[k] = p_alive[i] 
+            p_event[k] = p_event[i]
+            kept +=1
+            
+    num_part = kept
+    alive = num_part
+    # print("population start: {0}".format(alive_last))
+    # print("population end: {0}".format(alive_now))
     print("particles produced from fission: {0}".format(fissions_to_add))
     print("particles killed: {0}".format(killed))
     print("total particles now stored: {0}".format(num_part))
+    # alive_last = alive_now
     
-    alive_last = alive_now
-    
-#===============================================================================
-# EVENT 10 : Tally score
-#===============================================================================
-trans_rhs = 0
-for i in range(num_part):
-    if p_event[i] == 5:
-        trans_rhs += 1
+    g+=1
+
 
 print("")
-print("fraction of particles to transit: {0}".format(trans_rhs/num_part))
-
+print("leak left: {0}".format(trans_lhs/init_particle))
+print("")
+print("leak right: {0}".format(trans_rhs/init_particle))
+print("")
 print('')
 print("********************END SIMULATION********************")
 print('')
