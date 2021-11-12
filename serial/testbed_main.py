83aedb3ba0a059accd43fe0c4950c2a6b4a9103c
Jackson Morgan <67020956+jpmorgan98@users.noreply.github.com>
2021-11-12 15:22:03 -0800

    fixed culling index
    homogeneous fission now working

--- serial/testbed_main.py
+++ serial/testbed_main.py
@@ -4,7 +4,7 @@
 Name: Testbed_1
 breif: Event Based Transient MC for metaprograming exploration
 Author: Jackson Morgan (OR State Univ - morgjack@oregonstate.edu) CEMeNT
-Date: Nov 9th 2021
+Date: Nov 10th 2021
 current implemented physics:
         -slab geometry
         -multiregion (only one generating)
@@ -31,34 +31,34 @@ nu_new_neutrons = 2 #neutrons/fission
 isotropic = 1 #is it isotropic 1=yes 0=no
 
 # Test case 1
-cap_xsec = np.array([1/3]) #capture crossection
-scat_xsec = np.array([1/3]) #scattering crossection
-fis_xsec = np.array([1/3]) #fission crossection
-
-#abs_xsec = cap_xsec+fis_xsec #absorption crossection
-total_xsec = np.array([1]) #total crossection
-
-L = [0,2] #x coordinants of boundaries
-generation_region = 0
-regions = 1
-
-# Test case 2
-#index refers to region
-# cap_xsec = np.array([1/3, 1/3, 1/3]) #capture crossection
-# scat_xsec = np.array([2/3, 1/3, 2/3]) #scattering crossection
-# fis_xsec = np.array([0,1/3,0]) #fission crossection
+# cap_xsec = np.array([1/3]) #capture crossection
+# scat_xsec = np.array([1/3]) #scattering crossection
+# fis_xsec = np.array([1/3]) #fission crossection
 
 # #abs_xsec = cap_xsec+fis_xsec #absorption crossection
-# total_xsec = np.array([1,1,1]) #total crossection
-#L = [0,2,4,5] #coordinants of boundaries
-#generation_region = 1
-# regions = 3
+# total_xsec = np.array([1]) #total crossection
+
+# L = [0,2] #x coordinants of boundaries
+# generation_region = 0
+# regions = 1
+
+# Test case 2
+# index refers to region
+cap_xsec = np.array([1/3, 1/3, 1/3]) #capture crossection
+scat_xsec = np.array([2/3, 1/3, 2/3]) #scattering crossection
+fis_xsec = np.array([0,1/3,0]) #fission crossection
+
+#abs_xsec = cap_xsec+fis_xsec #absorption crossection
+total_xsec = np.array([1,1,1]) #total crossection
+L = [0,2,4,5] #coordinants of boundaries
+generation_region = 1
+regions = 3
 
 #===============================================================================
 # Allocate particle phase space
 #===============================================================================
 
-phase_parts = 5*num_part
+phase_parts = 5*num_part #see note about data storage
 
 # Position
 p_pos_x = np.zeros(phase_parts, dtype=np.float32)
@@ -107,8 +107,8 @@ norm_fis = fis_xsec/total_xsec
 # EVENT 0 : Sample particle sources
 #===============================================================================
 
-L_gen = L[1]-L[0]
-x_rhs_gen = L[0]
+L_gen = L[generation_region+1]-L[generation_region]
+x_rhs_gen = L[generation_region]
 
 for i in range(num_part):
     # Position
@@ -155,7 +155,7 @@ trans_rhs = 0
 while alive > 0:
     print("")
     print("===============================================================================")
-    print("                             Generation {0}".format(g))
+    print("                             Event Cycle {0}".format(g))
     print("===============================================================================")
     
     #===============================================================================
@@ -164,17 +164,16 @@ while alive > 0:
     killed = 0
     
     for i in range(num_part):
-        if p_alive[i] == True:
-            # Sample distance to collision
-            dist = -math.log(np.random.random())/total_xsec[p_region[i]]
+        # Sample distance to collision
+        dist = -math.log(np.random.random())/total_xsec[p_region[i]]
+    
+        # Move particle
+        p_pos_x[i] += p_dir_x[i]*dist
+        p_pos_y[i] += p_dir_y[i]*dist
+        p_pos_z[i] += p_dir_z[i]*dist
         
-            # Move particle
-            p_pos_x[i] += p_dir_x[i]*dist
-            p_pos_y[i] += p_dir_y[i]*dist
-            p_pos_z[i] += p_dir_z[i]*dist
-            
-            #advance particle clock
-            p_time[i]  += dist/p_speed[i]
+        #advance particle clock
+        p_time[i]  += dist/p_speed[i]
 
     #===============================================================================
     # EVENT 2 : Still in problem/ update region
@@ -184,16 +183,14 @@ while alive > 0:
     for i in range(num_part):
         #leak left
         if p_pos_x[i]<L[0]:
-            if p_alive[i] == True:
-                p_alive[i]=False
-                p_event[i]=6
-                #killed += 1
+            p_alive[i]=False
+            p_event[i]=6
+            #killed += 1
         #leak right
         elif p_pos_x[i]>L[1]:
-            if p_alive[i] == True:
-                p_event[i] = 5
-                p_alive[i] = False
-                #killed += 1
+            p_event[i] = 5
+            p_alive[i] = False
+            #killed += 1
         else:
             for l in range(regions):
                 if (L[l] < p_pos_x[i] < L[l+1]):
@@ -289,13 +286,19 @@ while alive > 0:
     #===============================================================================
     # EVENT 10 : Tally score
     #===============================================================================
-    for i in range(num_part):
-        if p_event[i] == 5:
-            trans_rhs += 1
-    for i in range(num_part):
-        if p_event[i] == 6:
-            trans_lhs += 1
+    flag_a = 0
+    flag_b = 0
     
+    for i in range(num_part):
+        if p_alive[i] == False:
+            if p_event[i] == 5:
+                trans_rhs += 1
+                flag_a = 1
+            elif p_event[i] == 6:
+                trans_lhs += 1
+                flag_b = 1
+        
+        
     
     #===============================================================================
     # Event 5: Purge the dead
@@ -303,27 +306,27 @@ while alive > 0:
     kept = 0
     for i in range(num_part):
         if p_alive[i] == True:
-            p_pos_x[k] = p_pos_x[i]
-            p_pos_y[k] = p_pos_y[i]
-            p_pos_z[k] = p_pos_z[i]
+            p_pos_x[kept] = p_pos_x[i]
+            p_pos_y[kept] = p_pos_y[i]
+            p_pos_z[kept] = p_pos_z[i]
             
             # Direction
-            p_dir_x[k] = p_dir_x[i]
-            p_dir_y[k] = p_dir_y[i]
-            p_dir_z[k] = p_dir_z[i]
+            p_dir_x[kept] = p_dir_x[i]
+            p_dir_y[kept] = p_dir_y[i]
+            p_dir_z[kept] = p_dir_z[i]
             
             # Speed
-            p_speed[k] = p_speed[i]
+            p_speed[kept] = p_speed[i]
             
             # Time
-            p_time[k] = p_time[i]
+            p_time[kept] = p_time[i]
             
             # Regions
-            p_region[k] = p_region[i]
+            p_region[kept] = p_region[i]
             
             # Flags
-            p_alive[k] = p_alive[i] 
-            p_event[k] = p_event[i]
+            p_alive[kept] = p_alive[i] 
+            p_event[kept] = p_event[i]
             kept +=1
             
     num_part = kept
