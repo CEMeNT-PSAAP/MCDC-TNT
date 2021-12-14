bd3e7fd956265db19e7ff5e73367ae4670753157
Jackson Morgan <67020956+jpmorgan98@users.noreply.github.com>
2021-12-13 16:05:14 -0800

    updated case 2
    3 region problem

--- serial/InputDeck.py
+++ serial/InputDeck.py
@@ -16,7 +16,7 @@ def SimulationSetup():
     #===============================================================================
     
     seed = 777
-    num_part = 100000 #number of particles to start
+    num_part = 1000 #number of particles to start
     particle_speed = 1 #particle speed
     
     # generations = 1
@@ -24,19 +24,35 @@ def SimulationSetup():
     isotropic = True #is it isotropic 1=yes 0=no
     
     # Test case 1
-    Lenght_slab = 1
-    surface_distances = np.array([0,Lenght_slab], dtype=np.float32)
+    # Lenght_slab = 1
+    # surface_distances = np.array([0,Lenght_slab], dtype=np.float32)
     
-    mesh_cell_length = 0.1 #dx
-    N_mesh = int(Lenght_slab/mesh_cell_length)
+    # mesh_cell_length = 0.1 #dx
+    # N_mesh = int(Lenght_slab/mesh_cell_length)
     
-    cap_xsec = 1/3 #capture crossection
-    scat_xsec = 1/3 #scattering crossection
-    fis_xsec = 1/3 #fission crossection
+    # cap_xsec = 1/3 #capture crossection
+    # scat_xsec = 1/3 #scattering crossection
+    # fis_xsec = 1/3 #fission crossection
+    
+    # #abs_xsec = cap_xsec+fis_xsec #absorption crossection
+    # total_xsec = 1 #total crossection
+    
+    
+    # Test case 2
+    
+    # Test case 2
+    # index refers to region
+    cap_xsec = np.array([1/3, 1/3, 1/3]) #capture crossection
+    scat_xsec = np.array([2/3, 1/3, 2/3]) #scattering crossection
+    fis_xsec = np.array([0,1/3,0]) #fission crossection
     
     #abs_xsec = cap_xsec+fis_xsec #absorption crossection
-    total_xsec = 1 #total crossection
+    total_xsec = np.array([1,1,1]) #total crossection
     
+    Lenght_slab = 3
+    surface_distances = np.array([0,1,2,Lenght_slab], dtype=np.float32)
+    mesh_cell_length = 0.1 #dx
+    N_mesh = int(Lenght_slab/mesh_cell_length)
     
     #establishing mesh
     mesh_scat_xsec = np.zeros(N_mesh, dtype=np.float32)
@@ -44,20 +60,14 @@ def SimulationSetup():
     mesh_fis_xsec = np.zeros(N_mesh, dtype=np.float32)
     mesh_total_xsec = np.zeros(N_mesh, dtype=np.float32)
     
-    for cell in range(N_mesh):
-        mesh_scat_xsec[cell] = scat_xsec
-        mesh_cap_xsec[cell] = cap_xsec
-        mesh_fis_xsec[cell] = fis_xsec
-        mesh_total_xsec[cell] = total_xsec
+    for i in range(len(surface_distances)-1):
+        for cell in range(int(surface_distances[i]/mesh_cell_length), int(surface_distances[i+1]/mesh_cell_length)):
+            mesh_scat_xsec[cell] = scat_xsec[i]
+            mesh_cap_xsec[cell] = cap_xsec[i]
+            mesh_fis_xsec[cell] = fis_xsec[i]
+            mesh_total_xsec[cell] = total_xsec[i]
+        
     
-    # # Test case 2
-    # # index refers to region
-    # cap_xsec = np.array([1/3, 1/3, 1/3]) #capture crossection
-    # scat_xsec = np.array([2/3, 1/3, 2/3]) #scattering crossection
-    # fis_xsec = np.array([0,1/3,0]) #fission crossection
-    
-    # #abs_xsec = cap_xsec+fis_xsec #absorption crossection
-    # total_xsec = np.array([1,1,1]) #total crossection
     # L = [0,2,4,5] #coordinants of boundaries
     # generation_region = 1
     # regions = 3
